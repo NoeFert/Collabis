506b7c30525ea5a892aa5b9d44138a4de71633f7
dev-ugo <dev.ugopreis@gmail.com>
2026-05-14 15:28:57 +0200

    fix edit post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -8,10 +8,12 @@ use App\Entity\Media;
 use App\Entity\UserProfile;
 use App\Form\PostType;
 use App\Repository\CategoryRepository;
+use App\Repository\ConversationRepository;
 use App\Repository\PostRepository;
 use App\Service\BlobStorage;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -60,29 +62,7 @@ final class PostController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
-            $categoryKey = $form->get('category')->getData() ?: 'demande';
-            $category = $this->findOrCreateCategory($categoryKey, $entityManager, $categoryRepository);
-            
-            foreach ($post->getCategories() as $existingCategory) {
-                $post->removeCategory($existingCategory);
-            }
-            $post->addCategory($category);
-
-            $imageFiles = $form->get('images')->getData();
-            if ($imageFiles) {
-                foreach ($imageFiles as $imageFile) {
-                    $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-                    $safeFilename = $slugger->slug($originalFilename);
-                    $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
-                    $imageUrl = $blobStorage->uploadPublic($imageFile, 'posts/'.$newFilename);
-
-                    $media = new Media();
-                    $media->setUrl($imageUrl);
-                    $media->setPost($post);
-                    $entityManager->persist($media);
-                }
-            }
+            $this->syncSubmittedPostData($post, $form, $entityManager, $slugger, $blobStorage, $categoryRepository);
 
             $entityManager->persist($post);
             $entityManager->flush();
@@ -100,6 +80,89 @@ final class PostController extends AbstractController
     public function show(Post $post): Response
     {
         return $this->render('post/show.html.twig', ['post' => $post]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function edit(Request $request, Post $post, EntityManagerInterface $entityManager, SluggerInterface $slugger, BlobStorage $blobStorage, CategoryRepository $categoryRepository): Response
+    {
+        if (!$this->isPostOwner($post)) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $category = $post->getCategories()->first() ?: null;
+        $form = $this->createForm(PostType::class, $post, [
+            'category_key' => $category ? $category->getCategoryKey() : 'demande',
+            'offer_type' => $post->getOfferType() ?: 'demande',
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->syncSubmittedPostData($post, $form, $entityManager, $slugger, $blobStorage, $categoryRepository);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post_show', ['id' => $post->getId()]);
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_post_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function delete(Request $request, Post $post, EntityManagerInterface $entityManager, ConversationRepository $conversationRepository): Response
+    {
+        if (!$this->isPostOwner($post)) {
+            throw $this->createAccessDeniedException();
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->request->get('_token'))) {
+            foreach ($conversationRepository->findBy(['post' => $post]) as $conversation) {
+                $conversation->setPost(null);
+            }
+
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_my_profile');
+    }
+
+    private function isPostOwner(Post $post): bool
+    {
+        $user = $this->getUser();
+
+        return $user instanceof UserProfile && $post->getUser()?->getId() === $user->getId();
+    }
+
+    private function syncSubmittedPostData(Post $post, FormInterface $form, EntityManagerInterface $entityManager, SluggerInterface $slugger, BlobStorage $blobStorage, CategoryRepository $categoryRepository): void
+    {
+        $categoryKey = $form->get('category')->getData() ?: 'demande';
+        $category = $this->findOrCreateCategory($categoryKey, $entityManager, $categoryRepository);
+
+        foreach ($post->getCategories()->toArray() as $existingCategory) {
+            $post->removeCategory($existingCategory);
+        }
+        $post->addCategory($category);
+
+        $imageFiles = $form->get('images')->getData();
+        if (!$imageFiles) {
+            return;
+        }
+
+        foreach ($imageFiles as $imageFile) {
+            $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+            $imageUrl = $blobStorage->uploadPublic($imageFile, 'posts/'.$newFilename);
+
+            $media = new Media();
+            $media->setUrl($imageUrl);
+            $media->setPost($post);
+            $entityManager->persist($media);
+        }
     }
 
     private function findOrCreateCategory(string $categoryKey, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository): Category
