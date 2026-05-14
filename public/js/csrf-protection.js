3b733f6ceef9d19b99dbb0710a9ed128a0c83a44
dev-ugo <dev.ugopreis@gmail.com>
2026-05-14 16:37:27 +0200

    fix importmap

---
+++ public/js/csrf-protection.js
@@ -0,0 +1,82 @@
+(function () {
+    const nameCheck = /^[-_a-zA-Z0-9]{4,22}$/;
+    const tokenCheck = /^[-_/+a-zA-Z0-9]{24,}$/;
+
+    function findCsrfField(formElement) {
+        return formElement.querySelector('input[data-controller="csrf-protection"], input[name="_csrf_token"]');
+    }
+
+    function generateCsrfToken(formElement) {
+        const csrfField = findCsrfField(formElement);
+
+        if (!csrfField) {
+            return;
+        }
+
+        let csrfCookie = csrfField.getAttribute('data-csrf-protection-cookie-value');
+        let csrfToken = csrfField.value;
+
+        if (!csrfCookie && nameCheck.test(csrfToken)) {
+            csrfField.setAttribute('data-csrf-protection-cookie-value', csrfCookie = csrfToken);
+            csrfField.defaultValue = csrfToken = btoa(String.fromCharCode.apply(
+                null,
+                (window.crypto || window.msCrypto).getRandomValues(new Uint8Array(18))
+            ));
+        }
+
+        csrfField.dispatchEvent(new Event('change', { bubbles: true }));
+
+        if (csrfCookie && tokenCheck.test(csrfToken)) {
+            const cookie = csrfCookie + '_' + csrfToken + '=' + csrfCookie + '; path=/; samesite=strict';
+            document.cookie = window.location.protocol === 'https:' ? '__Host-' + cookie + '; secure' : cookie;
+        }
+    }
+
+    function generateCsrfHeaders(formElement) {
+        const headers = {};
+        const csrfField = findCsrfField(formElement);
+
+        if (!csrfField) {
+            return headers;
+        }
+
+        const csrfCookie = csrfField.getAttribute('data-csrf-protection-cookie-value');
+
+        if (tokenCheck.test(csrfField.value) && nameCheck.test(csrfCookie)) {
+            headers[csrfCookie] = csrfField.value;
+        }
+
+        return headers;
+    }
+
+    function removeCsrfToken(formElement) {
+        const csrfField = findCsrfField(formElement);
+
+        if (!csrfField) {
+            return;
+        }
+
+        const csrfCookie = csrfField.getAttribute('data-csrf-protection-cookie-value');
+
+        if (tokenCheck.test(csrfField.value) && nameCheck.test(csrfCookie)) {
+            const cookie = csrfCookie + '_' + csrfField.value + '=0; path=/; samesite=strict; max-age=0';
+            document.cookie = window.location.protocol === 'https:' ? '__Host-' + cookie + '; secure' : cookie;
+        }
+    }
+
+    document.addEventListener('submit', function (event) {
+        generateCsrfToken(event.target);
+    }, true);
+
+    document.addEventListener('turbo:submit-start', function (event) {
+        const headers = generateCsrfHeaders(event.detail.formSubmission.formElement);
+
+        Object.keys(headers).forEach(function (key) {
+            event.detail.formSubmission.fetchRequest.headers[key] = headers[key];
+        });
+    });
+
+    document.addEventListener('turbo:submit-end', function (event) {
+        removeCsrfToken(event.detail.formSubmission.formElement);
+    });
+})();
